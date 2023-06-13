2a0c7d184faa5454893d32fbffe4387176c06c28
Peter Eldritch <peter.jirak@gmail.com>
2023-06-12 17:54:25 -0700

    Continuing the red-green-refactor

--- leap_year.py
+++ leap_year.py
@@ -1,4 +1,7 @@
 def leap_year(year):
-   if year % 4 != 0:
-      return False
-   return True
+    if year % 4 != 0:
+        return False
+    elif year % 100 == 0:
+        if year % 400 != 0:
+            return False
+    return True
--- test_leap_year.py
+++ test_leap_year.py
@@ -7,3 +7,10 @@ def test_can_invoke_leap_year_function():
 
 def test_leap_year_function_returns_false_for_years_not_divisible_by_four():
     assert leap_year(1983) == False
+
+def test_leap_year_function_returns_true_for_divisible_by_four_but_not_100_or_400():
+    assert leap_year(1984) == True
+
+def test_returns_false_for_divisible_by_4_and_divisible_by_100_but_not_400():
+    assert leap_year(1900) == False
+    
