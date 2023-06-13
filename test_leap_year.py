9164ce9c744ff3003dcb542466ca6e67d6092845
Peter Eldritch <peter.jirak@gmail.com>
2023-06-12 17:25:38 -0700

    Added a test for the requirement that leap_year return False for years not divisible by 4. It fails.

--- test_leap_year.py
+++ test_leap_year.py
@@ -4,3 +4,6 @@ from leap_year import leap_year
 
 def test_can_invoke_leap_year_function():
     leap_year(1)
+
+def test_leap_year_function_returns_false_for_years_not_divisible_by_four():
+    assert leap_year(1983) == False
